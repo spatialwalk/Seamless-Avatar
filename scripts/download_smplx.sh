12819f0f011a2c3fe4b4233ceb3edad569920d14
瓜江君 <10245735+guajiangjun@user.noreply.gitee.com>
2026-02-28 22:09:45 +0800

    update download smplx bug

--- scripts/download_smplx.sh
+++ scripts/download_smplx.sh
@@ -1,7 +1,7 @@
 urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
 COLOR='\033[0;32m'
 
-username and password input
+# username and password input
 
 echo -e "\nIf you do not have an account you can register at https://smpl-x.is.tue.mpg.de/ following the installation instruction."
 read -p "Username (smpl-x):" username
@@ -9,9 +9,9 @@ read -p "Password (smpl-x):" password
 username=$(urle $username)
 password=$(urle $password)
 
-mkdir -p model/smplx
+mkdir -p models/smplx
 
 wget --post-data "username=$username&password=$password" \
 'https://download.is.tue.mpg.de/download.php?domain=smplx&sfile=SMPLX_NEUTRAL_2020.npz&resume=1' \
--O model/smplx/SMPLX_NEUTRAL_2020.npz \
+-O models/smplx/SMPLX_NEUTRAL_2020.npz \
 --no-check-certificate --continue
